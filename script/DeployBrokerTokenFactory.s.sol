532f8f2b9d0e62786633f5bceed11ceca9a0cb00
Max Pushkarov <mpushkarov@yellow.org>
2024-08-03 14:10:31 +0300

    style: run `forge fmt`

--- script/DeployBrokerTokenFactory.s.sol
+++ script/DeployBrokerTokenFactory.s.sol
@@ -4,15 +4,14 @@ import {Script, console} from "forge-std/Script.sol";
 import {BrokerTokenFactory} from "../src/BrokerTokenFactory.sol";
 
 contract DeployBrokerTokenFactory is Script {
-  function run() public {
-    string memory ownerStr = vm.envString("ERC20_FACTORY_OWNER");
-    address owner = vm.parseAddress(ownerStr);
+    function run() public {
+        string memory ownerStr = vm.envString("ERC20_FACTORY_OWNER");
+        address owner = vm.parseAddress(ownerStr);
 
-    vm.startBroadcast(); // start broadcasting transactions to the blockchain
-    BrokerTokenFactory factory = new BrokerTokenFactory(owner);
-    vm.stopBroadcast();
+        vm.startBroadcast(); // start broadcasting transactions to the blockchain
+        BrokerTokenFactory factory = new BrokerTokenFactory(owner);
+        vm.stopBroadcast();
 
-    console.log("BrokerToken Factory address: %s", address(factory));
-  }
+        console.log("BrokerToken Factory address: %s", address(factory));
+    }
 }
-
