b2e44f66be9181176b192cb562aa80031d5de1b3
nksazonov <nsazonov@openware.com>
2024-09-23 17:05:13 +0300

    ref: forge fmt

--- src/vault/test/MockedAuthorizer.sol
+++ src/vault/test/MockedAuthorizer.sol
@@ -4,21 +4,13 @@ pragma solidity ^0.8.0;
 import "../../interfaces/IAuthorize.sol";
 
 contract TrueAuthorize is IAuthorize {
-    function authorize(
-        address,
-        address,
-        uint256
-    ) external pure override returns (bool) {
+    function authorize(address, address, uint256) external pure override returns (bool) {
         return true;
     }
 }
 
 contract FalseAuthorize is IAuthorize {
-    function authorize(
-        address,
-        address,
-        uint256
-    ) external pure override returns (bool) {
+    function authorize(address, address, uint256) external pure override returns (bool) {
         return false;
     }
 }
